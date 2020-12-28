91c6d11409cec37709651c75db08500a326542ab
bnbong <bbbong9@gmail.com>
2020-12-28 21:52:39 +0900

    account 모델에 follower, following, profile_image필드 추가

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,13 +1,24 @@
 from django.conf.urls import include, url
 from django.urls import path
 
-from .views import GoogleLogin, current_user, UserList
+from . import views
 
 app_name = 'accounts'
 
 urlpatterns = [
-    path('rest-auth/google/', GoogleLogin.as_view(), name='google_login'),
-    path('', UserList.as_view()),
-    path('current', current_user),
+    path('rest-auth/google/', views.GoogleLogin.as_view(), name='google_login'),
+    path('', views.UserList.as_view()),
+    path('current', views.current_user),
+
+    url(r'^search/$', views.Search.as_view(), name='search'),
+    url(r'^explore/$', views.ExploreUsers.as_view(), name='explore_user'),
+    url(r'^(?P<username>\w+)/$', views.UserProfile.as_view(), name='user_profile'),
+    url(r'^(?P<user_id>\d+)/follow/$', views.FollowUser.as_view(), name='follow_user'),
+    url(r'^(?P<user_id>\d+)/unfollow/$', views.UnFollowUser.as_view(), name='unfollow_user'),
+    url(r'^(?P<username>\w+)/followers/$', views.UserFollowers.as_view(), name='user_followers'),
+    url(r'^(?P<username>\w+)/following/$', views.UserFollowing.as_view(), name='user_following'), # 로그인 되어 있는 유저가 팔로우한 다른 유저목록
+    # url(r'^(?P<user_id>\d+)/makefriend/$', views.MakeFriend.as_view(), name='friend_user'),
+    # url(r'^(?P<user_id>\d+)/deletefriend/$', views.DeleteFriend.as_view(), name='unfriend_user'),
+    # url(r'^(?P<username>\w+)/friends/$', views.UserFriends.as_view(), name='user_friends'),
     # url accounts/registration을 django-rest 라이브러리가 아니라 클라이언트로 연결시킬 예정
 ]
--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,4 +1,5 @@
 from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
+from django.contrib.auth import get_user_model
 from rest_auth.registration.views import SocialLoginView
 from allauth.socialaccount.providers.oauth2.client import OAuth2Client
 
@@ -6,7 +7,10 @@ from rest_framework import permissions, status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from .serializers import UserSerializer, UserSerializerWithToken
+
+from .serializers import UserSerializer, UserSerializerWithToken, UserProfileSerializer
+
+User = get_user_model()
 
 
 class GoogleLogin(SocialLoginView):
@@ -16,7 +20,6 @@ class GoogleLogin(SocialLoginView):
 
 @api_view(['GET'])
 def current_user(request):
-
     serializer = UserSerializer(request.user)
     return Response(serializer.data)
 
@@ -31,3 +34,199 @@ class UserList(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserProfile(APIView):
+    """
+    Check Current UserProfile
+    """
+
+    def get_user(self, username):
+        try:
+            user = User.objects.get(username=username)
+            return user
+        except User.DoesNotExist:
+            return None
+
+    def get(self, request, username, format=None):
+
+        user = self.get_user(username)
+
+        if user is None:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        serializer = UserProfileSerializer(user)
+
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+    def put(self, request, username, format=None):
+
+        req_user = request.user
+
+        user = self.get_user(username)
+
+        if user is None:
+
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        elif user.username != req_user.username:
+
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
+
+        else:
+
+            serializer = UserProfileSerializer(user, data=request.data, partial=True)
+
+            if serializer.is_valid():
+
+                serializer.save()
+
+                return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+            else:
+
+                return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ExploreUsers(APIView):
+    """
+    신규 가입 유저 5명을 탐색한다.
+    """
+    def get(self, request, format=None):
+        last_five = User.objects.all().order_by('-date_joined')[:5]
+        serializer = UserSerializer(last_five, many=True)
+
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+
+class FollowUser(APIView):
+
+    def post(self, request, user_id, format=None):
+        user = request.user
+
+        try:
+            user_to_follow = User.objects.get(id=user_id)
+        except User.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        user.following.add(user_to_follow)
+        user_to_follow.followers.add(user)
+
+        return Response(status=status.HTTP_200_OK)
+
+
+class UnFollowUser(APIView):
+
+    def post(self, request, user_id, format=None):
+
+        user = request.user
+
+        try:
+            user_to_follow = User.objects.get(id=user_id)
+        except User.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        user.following.remove(user_to_follow)
+        user_to_follow.followers.remove(user)
+
+        return Response(status=status.HTTP_200_OK)
+
+
+class Search(APIView):
+
+    def get(self, request, format=None):
+
+        username = request.query_params.get('username', None)
+
+        if username is not None:
+
+            users = User.objects.filter(username__istartswith=username)
+
+            serializer = UserSerializer(users, many=True)
+
+            return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+        else:
+
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserFollowers(APIView):
+
+    def get(self, request, username, format=None):
+
+        try:
+            user = User.objects.get(username=username)
+        except User.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        followers = user.followers.all()
+
+        serializer = UserSerializer(followers, many=True)
+
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+
+class UserFollowing(APIView):
+
+    def get(self, request, username, format=None):
+
+        try:
+            user = User.objects.get(username=username)
+        except User.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        following = user.following.all()
+
+        serializer = UserSerializer(following, many=True)
+
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+
+# class MakeFriend(APIView):
+#
+#     def post(self, request, user_id, format=None):
+#         user = request.user
+#
+#         try:
+#             user_to_friends = User.objects.get(id=user_id)
+#         except User.DoesNotExist:
+#             return Response(status=status.HTTP_404_NOT_FOUND)
+#
+#         user.friends.add(user_to_friends)
+#         user_to_friends.friends.add(user)
+#
+#         return Response(status=status.HTTP_200_OK)
+#
+#
+# class DeleteFriend(APIView):
+#
+#     def post(self, request, user_id, format=None):
+#
+#         user = request.user
+#
+#         try:
+#             user_to_friends = User.objects.get(id=user_id)
+#         except User.DoesNotExist:
+#             return Response(status=status.HTTP_404_NOT_FOUND)
+#
+#         user.friends.remove(user_to_friends)
+#         user_to_friends.friends.remove(user)
+#
+#         return Response(status=status.HTTP_200_OK)
+#
+#
+# class UserFriends(APIView):
+#
+#     def get(self, request, username, format=None):
+#
+#         try:
+#             user = User.objects.get(username=username)
+#         except User.DoesNotExist:
+#             return Response(status=status.HTTP_404_NOT_FOUND)
+#
+#         friends = user.friends.all()
+#
+#         serializer = UserSerializer(friends, many=True)
+#
+#         return Response(data=serializer.data, status=status.HTTP_200_OK)
