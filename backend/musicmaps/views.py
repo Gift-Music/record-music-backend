32c26d82713695b6105c119a2e06eb6d5c94c732
Packet <199sd@naver.com>
2021-01-27 17:13:25 +0900

    Music app 추가

--- backend/musicmaps/views.py
+++ backend/musicmaps/views.py
@@ -6,7 +6,7 @@ from rest_framework.response import Response
 import json
 
 
-def get_music_maps(coordinates):
+def get_music_maps_geo(coordinates):
     s = Post.search(index='musicmaps').filter({
         'geo_distance': {
             "distance": "1km",
@@ -19,6 +19,18 @@ def get_music_maps(coordinates):
     return response
 
 
+def create_music_maps(data):
+    post = Post(
+        open_range=data.open_range,
+        author_id=data.author_id,
+        comments_on=data.comments_on,
+        content=data.content,
+        coordinates=data.coordinates,
+        street_address=data.address,
+        building_number=data.build_num
+    )
+
+
 class MusicMapsGeo(APIView):
     """
     Get MusicMaps by Coordinates
@@ -26,8 +38,19 @@ class MusicMapsGeo(APIView):
 
     def get(self, request, format=None):
         body = json.loads(request.body)
-        res = get_music_maps(body['coordinates'])
+        res = get_music_maps_geo(body['coordinates'])
         if res.hits.total.value == 0:
             return Response(status=status.HTTP_404_NOT_FOUND)
         else:
             return Response(data=res, status=status.HTTP_200_OK)
+
+class MusicMaps(APIView):
+    """
+    CRUD MusicMaps by Music and Address
+    """
+
+    def get(self, query):
+        pass
+
+    def post(self, request):
+        pass
