e106209dd01bf3840133e7a4aa951b77ee277c41
Packet <199sd@naver.com>
2021-01-28 18:35:43 +0900

    Music Model 추가

--- backend/music/models.py
+++ backend/music/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Music(models.Model):
+    artists = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+    yt_song_id = models.CharField(max_length=30)
+    cover_image = models.ImageField(upload_to="../images")
